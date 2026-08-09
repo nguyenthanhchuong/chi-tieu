7b1e9e31b0b451ac679b760dd5e91fa10effa9ed
chuong <chuongnt@utop.io>
2026-08-09 23:32:58 +0700

    Restyle app in pastel pink theme

--- sw.js
+++ sw.js
@@ -6,12 +6,12 @@
 // vĩnh viễn chạy bản cũ, mọi bản sửa đẩy lên đều không tới được người dùng.
 // App này luôn cần mạng để lấy dữ liệu từ Google, nên ưu tiên mạng không làm
 // mất gì, mà lại bảo đảm người dùng luôn chạy bản mới nhất.
-const CACHE_VERSION = "chi-tieu-v7";
+const CACHE_VERSION = "chi-tieu-v8";
 const SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=7",
-  "./app.js?v=7",
+  "./style.css?v=8",
+  "./app.js?v=8",
   "./manifest.json"
 ];
 
