c10730851fda0fd1a00feae0db82518ce4696fcb
chuong <chuongnt@utop.io>
2026-08-14 13:36:42 +0700

    v17: switch palette to Hong dao and consolidate colours into CSS variables

--- sw.js
+++ sw.js
@@ -6,13 +6,13 @@
 // vĩnh viễn chạy bản cũ, mọi bản sửa đẩy lên đều không tới được người dùng.
 // App này luôn cần mạng để lấy dữ liệu từ Google, nên ưu tiên mạng không làm
 // mất gì, mà lại bảo đảm người dùng luôn chạy bản mới nhất.
-const CACHE_VERSION = "chi-tieu-v16";
+const CACHE_VERSION = "chi-tieu-v17";
 const SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=16",
-  "./logic.js?v=16",
-  "./app.js?v=16",
+  "./style.css?v=17",
+  "./logic.js?v=17",
+  "./app.js?v=17",
   "./manifest.json"
 ];
 
